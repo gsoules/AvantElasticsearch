2ab946fa88f093d2ea4fb450171ef683f5f92854
gsoules <gsoules@avantlogic.com>
2019-04-08 15:18:15 -0400

    Move facet construction logic here from AvantElasticsearchIndexBuilder.

--- models/AvantElasticsearchFacets.php
+++ models/AvantElasticsearchFacets.php
@@ -18,6 +18,62 @@ class AvantElasticsearchFacets extends AvantElasticsearch
         return $queryString;
     }
 
+    public function constructFacets($elementName, $elasticsearchFieldName, $texts, &$facets)
+    {
+        $facetValues = array();
+        foreach ($texts as $text)
+        {
+//                    if ($elementName == 'Type' || $elementName == 'Subject')
+//                    {
+//                        // Find the first comma.
+//                        $needle = ', ';
+//                        $pos1 = strpos($text, $needle);
+//                        if ($pos1 !== false)
+//                        {
+//                            $pos2 = strpos($text, $needle, $pos1 + strlen($needle));
+//                            if ($pos2 !== false) {
+//                                // Filter out the ancestry to leave just the root text.
+//                                $text = trim(substr($text, 0, $pos2));
+//                            }
+//                        }
+//                        $facetValues[] = $text;
+//                    }
+            if ($elementName == 'Place' || $elementName == 'Type' || $elementName == 'Subject')
+            {
+                // Find the last comma.
+                $index = strrpos($text, ',', -1);
+                if ($index !== false)
+                {
+                    // Filter out the ancestry to leave just the leaf text.
+                    $text = trim(substr($text, $index + 1));
+                }
+                $facetValues[] = $text;
+            }
+            else if ($elementName == 'Date')
+            {
+                // This code is only called if Date element is not empty.
+                // As such, we can't use it to create an "Unknown date" facet value.
+                $year = '';
+                if (preg_match("/^.*(\d{4}).*$/", $text, $matches))
+                {
+                    $year = $matches[1];
+                }
+
+                if (!empty($year))
+                {
+                    $decade = $year - ($year % 10);
+                    $facetValues[] = $decade . "'s";
+                }
+            }
+
+            $facetValuesCount = count($facetValues);
+            if ($facetValuesCount >= 1)
+            {
+                $facets[$elasticsearchFieldName] = $facetValuesCount > 1 ? $facetValues : $facetValues[0];
+            }
+        }
+    }
+
     public function convertFacetValuesToString($value)
     {
         return is_array($value) ? implode(', ', $value) : $value;
