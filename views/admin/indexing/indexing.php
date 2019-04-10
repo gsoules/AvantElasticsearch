67b8f08540dafde311ed99601b339035d3491adb
gsoules <gsoules@avantlogic.com>
2019-04-10 09:51:58 -0400

    Add memory usage statistics.

--- views/admin/indexing/indexing.php
+++ views/admin/indexing/indexing.php
@@ -1,5 +1,5 @@
 <?php
-ini_set('max_execution_time', 300);
+ini_set('max_execution_time', 1000);
 
 $pageTitle = __('Elasticsearch Indexing');
 echo head(array('title' => $pageTitle, 'bodyclass' => 'indexing'));
@@ -11,18 +11,31 @@ if (isset($_REQUEST['export']))
     $export = (bool) $_REQUEST['export'];
     $limit = isset($_REQUEST['limit']) ? intval($_REQUEST['limit']) : 25;
     $avantElasticsearchIndexBuilder = new AvantElasticsearchIndexBuilder();
+
+    $mem1 = memory_get_usage();
     $responses = $avantElasticsearchIndexBuilder->indexAll($export, $limit);
+    $mem2 = memory_get_usage();
+
+    $mb = 1048576;
+    $used = intval(($mem2 - $mem1) / $mb);
+    $current = intval($mem2 /  $mb);
+    $peak = intval(memory_get_peak_usage() /  $mb);
+
     $message = $avantElasticsearchIndexBuilder->convertResponsesToMessageString($responses);
 
     if (empty($message))
     {
-        echo '<p>' . 'SUCCESS' . '</p>';
+        echo "<p>SUCCESS</p>";
     }
     else
     {
-        echo '<p>' . 'ERRORS' . '</p>';
-        echo '<p>' . $message . '</p>';
+        echo "<p>ERRORS</p>";
+        echo "<p>$message</p>";
     }
+
+    echo "<p>Memory used: $used MB</p>";
+    echo "<p>Current usage: $current MB</p>";
+    echo "<p>Peak usage: $peak MB</p>";
 }
 
 echo foot();
