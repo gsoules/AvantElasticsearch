d447d5efc1545c6ee59e19cd1480d6d5f2ae6bbd
gsoules <gsoules@avantlogic.com>
2019-04-15 18:26:43 -0400

    Add UI controls to the indexing page.

--- views/admin/indexing/indexing.php
+++ views/admin/indexing/indexing.php
@@ -3,30 +3,50 @@ ini_set('max_execution_time', 1200);
 
 $pageTitle = __('Elasticsearch Indexing');
 echo head(array('title' => $pageTitle, 'bodyclass' => 'indexing'));
+$operation = isset($_REQUEST['operation']) ? $_REQUEST['operation'] : 'none';
+$limit = isset($_REQUEST['limit']) ? intval($_REQUEST['limit']) : 100;
+$options = array('none' => 'No Action', 'import' =>'Import', 'export' => 'Export');
+$action = url("elasticsearch/indexing?operation=$operation&limit=$limit");
+$file = ElasticsearchConfig::getOptionValueForExportFile();
 
-echo '<h4>' . __('Indexer') . '</h4>';
+echo "<form id='indexing-form' name='indexing-form' action='$action' method='get'>";
+echo '<div class="indexing-radio-buttons">';
+echo $this->formRadio('operation', $operation, null, $options);
+echo '</div>';
+echo '<div class="">Limit: ';
+echo $this->formText('limit', $limit, array('size' => '4', 'id' => 'limit'));
+echo '</div>';
+echo "<button id='submit_index' 'type='submit' value='Index'>Start Indexing</button>";
+echo '</form>';
+echo "<div>$file</div>";
 
-if (isset($_REQUEST['export']))
+$mb = 1048576;
+$mem1 = intval(memory_get_usage() / $mb);
+$message = '';
+
+if ($operation != 'none')
 {
-    $export = (bool) $_REQUEST['export'];
-    $limit = isset($_REQUEST['limit']) ? intval($_REQUEST['limit']) : 25;
+    $export = $operation == 'export';
     $avantElasticsearchIndexBuilder = new AvantElasticsearchIndexBuilder();
-
-    $mb = 1048576;
-    $mem1 = intval(memory_get_usage() / $mb);
-
-    // Perform indexing.
     $responses = $avantElasticsearchIndexBuilder->indexAll($export, $limit);
-
-    $mem2 = intval(memory_get_usage() / $mb);
-
-    $used = intval($mem2 - $mem1);
-    $peak = intval(memory_get_peak_usage() /  $mb);
-
-    $executionTime = intval(microtime(true) - $_SERVER["REQUEST_TIME_FLOAT"]);
-
     $message = $avantElasticsearchIndexBuilder->convertResponsesToMessageString($responses);
+}
 
+$executionTime = intval(microtime(true) - $_SERVER["REQUEST_TIME_FLOAT"]);
+$mem2 = intval(memory_get_usage() / $mb);
+$used = intval($mem2 - $mem1);
+$peak = intval(memory_get_peak_usage() /  $mb);
+
+echo '<div>';
+echo "Start/End usage: $mem1 MB / $mem2 MB</br>";
+echo "Memory used: $used MB</br>";
+echo "Peak usage: $peak MB</br>";
+echo '</div>';
+
+if ($operation != 'none')
+{
+    echo '<hr/>';
+    echo "<div>$options[$operation] execution time: $executionTime seconds</div>";
     if (empty($message))
     {
         echo "<p>SUCCESS</p>";
@@ -36,11 +56,6 @@ if (isset($_REQUEST['export']))
         echo "<p>ERRORS</p>";
         echo "<p>$message</p>";
     }
-
-    echo "<p>Memory used: $used MB</p>";
-    echo "<p>Start/End usage: $mem1 MB / $mem2 MB</p>";
-    echo "<p>Peak usage: $peak MB</p>";
-    echo "<p>Execution time: $executionTime seconds</p>";
 }
 
 echo foot();
